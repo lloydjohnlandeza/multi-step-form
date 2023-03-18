b2a241f942d784f738dd809cf97847ba4f5a977d
Lloyd John Landeza <lloydjohnlandeza@gmail.com>
2023-03-18 18:15:10 +0800

    initial commit

---
+++ src/components/Stepper.vue
@@ -0,0 +1,66 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="stepper-container">
+    <ul>
+      <li>
+        <div>
+          1
+        </div>
+      </li>
+      <li>
+        <div>
+          2
+        </div>
+      </li>
+      <li>
+        <div>
+          3
+        </div>
+      </li>
+      <li>
+        <div>
+          4
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  .stepper-container {
+    display: flex;
+    aspect-ratio: 375 / 172;
+    background: url('../assets/bg-sidebar-mobile.svg') no-repeat center center/cover;
+  }
+  ul {
+    display: flex;
+    gap: 10px;
+    color: var(--white);
+    li {
+      > div {
+        transition-duration: var(--transition-duration);
+        border-radius: 100%;
+        border: solid 1px var(--light-blue);
+        width: 35px;
+        height: 35px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        &:hover {
+          font-weight: 700;
+          background-color: var(--light-blue);
+          color: var(--marine-blue);
+        }
+      }
+    }
+  }
+  @media (max-width: 1024px) {
+    ul {
+      margin: 30px auto;
+    }
+  }
+
+</style>
+
---
+++ src/views/Form.vue
@@ -0,0 +1,20 @@
+<template>
+  <main>
+    <Stepper />
+    <Step />
+  </main>
+</template>
+
+<script setup lang="ts">
+import Stepper from '../components/Stepper.vue'
+import Step from '../components/Step.vue'
+</script>
+
+<style scoped>
+  main {
+    max-width: 1440px;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+  }
+</style>
